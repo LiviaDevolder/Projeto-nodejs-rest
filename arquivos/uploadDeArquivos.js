49e9b83562bf3c8b538f8b8f0ca33b51e88c215c
Lívia BRQ <liviadevolder@gmail.com>
2021-07-10 12:08:28 -0300

    Aula 2

--- arquivos/uploadDeArquivos.js
+++ arquivos/uploadDeArquivos.js
@@ -1,5 +1,25 @@
 const fs = require('fs')
+const path = require('path')
 
-fs.createReadStream('./assets/peteca.jpg')
-  .pipe(fs.createWriteStream('./assets/peteca-stream.jpg'))
-  .on('finish', () => console.log('Imagem foi escrita com sucesso!'))
+module.exports = (caminho, nomeDoArquivo, callbackImagemCriada) => {
+  const tiposValidos = [
+    'jpg',
+    'jpeg',
+    'png'
+  ]
+  const tipo = path.extname(caminho)
+
+  const tipoEhValido = tiposValidos.indexOf(tipo.substring(1)) !== -1
+
+  if(!tipoEhValido) {
+    const erro = 'Tipo é inválido'
+    console.log('Erro! Extensão de arquivo não suportada')
+    callbackImagemCriada(erro)
+  } else {
+    const novoCaminho = `./assets/imagens/${nomeDoArquivo}${tipo}`
+
+    fs.createReadStream(caminho)
+      .pipe(fs.createWriteStream(novoCaminho))
+      .on('finish', () => callbackImagemCriada(false, novoCaminho))
+  }
+}
